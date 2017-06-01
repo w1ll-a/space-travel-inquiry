415267c823a219bfe89976a87235286cdd1ebe30
wllmscttandrsn <wllmscttandrsn@gmail.com>
2017-06-01 09:04:24 -0400

    add info

---
+++ Info.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+ 
+public class Info
+{
+  private String query, file, info;
+  
+	public Info(String query, String file){
+		this.query = query;
+		this.file = file;
+		Scanner data = new Scanner(new File(file));
+		while (data.hasNextLine()){
+      info = info “\n” + data.nextLine();
+    }
+  }
+	
+	public String getQuery(){
+		return query;
+	}
+ 
+	public String getInfo{
+		return info;
+	}
+ 
+}
