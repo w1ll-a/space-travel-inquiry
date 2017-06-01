d4ccc1a88f43c46a365c084d1fca8589455a5058
wllmscttandrsn <wllmscttandrsn@gmail.com>
2017-06-01 08:58:16 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	private ArrayList<Info> list = new ArrayList<Info>();
+ 
+  public Main(){
+		Window w = new Window(1000, 800);
+	}
+ 
+	public ArrayList<Info> addToList(Info i){
+    list.add(i);
+  }
+ 
+	public Info search(String query){
+		for(Info i : list){
+			if (i.getQuery == query) return i;
+		}
+		return null;
+  }
+}
