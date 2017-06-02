3aa9dfcbe0157b6a246af89436ae551a850d43f7
wllmscttandrsn <wllmscttandrsn@gmail.com>
2017-06-02 13:57:43 -0400

    InfoWindow + other window changes

---
+++ InfoWindow.java
@@ -0,0 +1,20 @@
+public class InfoWindow extents Window
+{
+  public InfoWindow(int x, int y, String title){
+    super(x, y, title);
+  }
+  
+  public void format(){
+		JPanel bl = new JPanel(new BorderLayout());
+		JTextField tf = new JTextField();
+		JButton done = new JButton(“Done”);
+		
+    frame.add(bl);
+    bl.add(tf, BorderLayout.NORTH);
+    bl.add(done, BorderLayout.SOUTH);
+	}
+  
+  public void addInfo(Info i){
+    tf.setText(i.getInfo());
+  }
+}
--- Window.java
+++ Window.java
@@ -1,31 +1,36 @@
 import javax.swing.*;
 
-public class Window extends JFrame
+public class Window extends JFrame implements ActionListener
 {
-	public Window(int x, int y){
-		JFrame frame = new JFrame(“Space Travel”);
+	public Window(int x, int y, String title){
+		JFrame frame = new JFrame(title);
 		frame.setSize(x, y);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultLookAndFeelDecorated(true);
+		
+		frame.format();
 	}
  
 	public void changeSize(int x, int y){
 		frame.setSize(x, y);
 	}
  
-	public void format(String type){
-		if (type == “data”){
-			JPanel bl = new JPanel(new BorderLayout());
-			JTextField tf = new JTextField();
-			JButton done = new JButton(“Done”);
-			
-      frame.add(bl);
-      bl.add(tf, BorderLayout.NORTH);
-      bl.add(done, BorderLayout.SOUTH);
-		}
-		if (type == “menu”){
-			JPanel header = new JPanel();
-      JPanel gl = new JPanel(new GridLayout(2, 3));
-			
-			frame.add(header);
-      frame.add(gl);
+	public void format(){
+		JPanel header = new JPanel();
+		JPanel gl = new JPanel(new GridLayout(2, 3));
+		
+		JTextArea headerText = new JTextArea("The History of Space Travel");
+		header.add(headerText);
+		
+		this.add(header);
+		this.add(gl);
+		
+		this.pack();
+		this.setVisible(true);
   }
+	
+	public void actionPerformed(ActionEvent e){
+		InfoWindow iw = new InfoWindow(800, 600);
+		iw.addInfo();
+	}
 }
