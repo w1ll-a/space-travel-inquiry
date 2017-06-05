5cf33c0a1f8cf543593b781b62fa82733cc499e5
Will Anderson <wllmscttandrsn@gmail.com>
2017-06-05 16:38:39 -0400

    added info to data

--- src/Data.java
+++ src/Data.java
@@ -2,11 +2,22 @@ import javax.swing.*;
 
 public class Data {
     public static Info earlyTrav = new Info("early",
-            "Early on in the Space Race during the Cold War, Russia was in the lead in all fronts. The first man in space, Yuri A. Gagarin, was Russian. Aboard the Vostok 1, " +
-                    "a modified SS-6 Sapwood ballistic missile, he reached a maximum altitude of 203 miles above Earth and orbited Earth fully once in his 108-minute flight.",
-            new JLabel(new ImageIcon("C:\\Users\\William\\Documents\\GitHub\\space-travel-inquiry\\media\\vostok-1_launch.jpg")));
+            "On October 4, 1957, the Soviet satellite Sputnik I became the first artificial satellite in Earth’s atmosphere, followed by the US’s Explorer I in 1958. Explorer I had " +
+                    "an experiment from a physicist from the University of Iowa designed to scan for radiation zones in Earth’s atmosphere. Because of the success of the experiment, the US " +
+                    "Congress authorized the creation of the National Aeronautics and Space Agency (NASA) later that year. Early on in the Space Race during the Cold War, Russia was in the " +
+                    "lead in all fronts. The first man in space, Yuri A. Gagarin, was Russian. Aboard the Vostok 1, a modified SS-6 Sapwood ballistic missile, he reached a maximum altitude " +
+                    "of 203 miles above Earth and orbited Earth fully once in his 108-minute flight. The US’s first attempt at this was much less successful, with Alan Shepard Jr. reaching " +
+                    "a maximum altitude of only 116 miles and not completing a complete orbit around Earth in the space capsule Freedom 7. He received military flight pay of $14.38 for his " +
+                    "5,180 mph trip into space.",
+            new JLabel(new ImageIcon("media\\vostok1launch.jpg")));
 
     public static Info coldWar = new Info("cold",
-            "",
-            new JLabel(new ImageIcon("C:\\Users\\William\\Documents\\GitHub\\space-travel-inquiry\\media\\us moon landing.jpg")));
+            "The Space Race, which was the race between the Soviet Union and the United States to the moon, was a major part of the Cold War that the United States ended up winning. " +
+                    "In the beginning, the Soviet Union was apparently ahead, with the launch of Sputnik I before the US’s Explorer I. However, President John F. Kennedy’s famous speech " +
+                    "about space travel and its importance, space travel was prioritized much more and the US started gaining on the Soviet Union. The US’s Explorer I won over government " +
+                    "support, and later in 1958 the government authorized the creation of the National Aeronautics and Space Agency (NASA). The Space Race ultimately ended with the US moon " +
+                    "landing, where Neil Armstrong and Edwin “Buzz” Aldrin became the first men on the moon, and were returned to Earth safely as promised in Kennedy’s speech. However, what " +
+                    "the US put in time and money into getting to the moon and back, Soviet Union put into space stations. The Soviet government wanted to focus on space flight for spying " +
+                    "rather than manned space travel, so they tasked their scientists with designing a space station capable of housing multiple astronauts.",
+            new JLabel(new ImageIcon("media\\usmoonlanding.jpg")));
 }
