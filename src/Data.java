4ea1a851d9dd6ac6dfd4f91545426b8cbd9b7e0e
Will Anderson <wllmscttandrsn@gmail.com>
2017-06-06 08:27:59 -0400

    added sources

--- src/Data.java
+++ src/Data.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
 public class Data {
-    public static Info earlyTrav = new Info("early",
+    public static Info earlyTrav = new Info(
             "On October 4, 1957, the Soviet satellite Sputnik I became the first artificial satellite in Earth’s atmosphere, followed by the US’s Explorer I in 1958. Explorer I had " +
                     "an experiment from a physicist from the University of Iowa designed to scan for radiation zones in Earth’s atmosphere. Because of the success of the experiment, the US " +
                     "Congress authorized the creation of the National Aeronautics and Space Agency (NASA) later that year. Early on in the Space Race during the Cold War, Russia was in the " +
@@ -10,7 +10,7 @@ public class Data {
                     "a maximum altitude of only 116 miles and not completing a complete orbit around Earth in the space capsule Freedom 7. He received military flight pay of $14.38 for his " +
                     "5,180 mph trip into space.");
 
-    public static Info coldWar = new Info("cold",
+    public static Info coldWar = new Info(
             "The Space Race, which was the race between the Soviet Union and the United States to the moon, was a major part of the Cold War that the United States ended up winning. " +
                     "In the beginning, the Soviet Union was apparently ahead, with the launch of Sputnik I before the US’s Explorer I. However, President John F. Kennedy’s famous speech " +
                     "about space travel and its importance, space travel was prioritized much more and the US started gaining on the Soviet Union. The US’s Explorer I won over government " +
@@ -18,4 +18,26 @@ public class Data {
                     "landing, where Neil Armstrong and Edwin “Buzz” Aldrin became the first men on the moon, and were returned to Earth safely as promised in Kennedy’s speech. However, what " +
                     "the US put in time and money into getting to the moon and back, Soviet Union put into space stations. The Soviet government wanted to focus on space flight for spying " +
                     "rather than manned space travel, so they tasked their scientists with designing a space station capable of housing multiple astronauts.");
+    
+    public static Info sources = new Info(
+            "SOURCES\nUSH in context: http://ic.galegroup.com/ic/uhic/searchResults/actionWin?scanId=CSH&query=OQE+space+travel&prodId=UHIC&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&totalSearchResultCount=1931&limiter=&contentModules=&displayGroups=&display-query=OQE+space+travel&action=e&windowstate=normal&activityType=BasicSearch&resetBreadCrumb=true&failOverType=&commentary=""
+            +
+            "\nCassini: https://www.nasa.gov/mission_pages/cassini/main/index.html https://saturn.jpl.nasa.gov/legacy/files/Cassini_Grand_Finale_Fact_Sheet_508.pdf"
+            +
+            "\nJuno: https://www.nasa.gov/mission_pages/juno/main/index.html https://www.nasa.gov/mission_pages/juno/overview/index.html"
+            +
+            "\nApollo 11: http://ic.galegroup.com/ic/uhic/ReferenceDetailsPage/ReferenceDetailsWindow?disableHighlighting=false&displayGroupName=Reference&currPage=&scanId=&query=&source=&prodId=UHIC&search_within_results=&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&limiter=&display-query=&displayGroups=&contentModules=&action=e&sortBy=&documentId=GALE%7CCV2643450738&windowstate=normal&activityType=&failOverType=&commentary=#"
+            +
+            "\nColombia Failure: http://ic.galegroup.com/ic/uhic/ReferenceDetailsPage/ReferenceDetailsWindow?disableHighlighting=false&displayGroupName=Reference&currPage=&scanId=&query=&source=&prodId=UHIC&search_within_results=&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&limiter=&display-query=&displayGroups=&contentModules=&action=e&sortBy=&documentId=GALE%7CEJ2359030032&windowstate=normal&activityType=&failOverType=&commentary="
+            +
+            "\nList of Early Space Achievements (Project Mercury): http://ic.galegroup.com/ic/uhic/ReferenceDetailsPage/ReferenceDetailsWindow?disableHighlighting=false&displayGroupName=Reference&currPage=&scanId=&query=&source=&prodId=UHIC&search_within_results=&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&limiter=&display-query=&displayGroups=&contentModules=&action=e&sortBy=&documentId=GALE%7CCX3468302505&windowstate=normal&activityType=&failOverType=&commentary="
+            +
+            "\nTimeline of Space Travel: http://www.greatachievements.org/?id=3642"
+            +
+            "\nPrimary of the First Satellite: http://ic.galegroup.com/ic/uhic/PrimarySourcesDetailsPage/PrimarySourcesDetailsWindow?disableHighlighting=false&displayGroupName=PrimarySources&currPage=&scanId=&query=&source=&prodId=UHIC&search_within_results=&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&limiter=&display-query=&displayGroups=&contentModules=&action=e&sortBy=&documentId=GALE%7CCX3441400082&windowstate=normal&activityType=&failOverType=&commentary="
+            +
+            "\nPrimary US space shuttles: http://ic.galegroup.com/ic/uhic/PrimarySourcesDetailsPage/PrimarySourcesDetailsWindow?disableHighlighting=false&displayGroupName=PrimarySources&currPage=&scanId=&query=&source=&prodId=UHIC&search_within_results=&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&limiter=&display-query=&displayGroups=&contentModules=&action=e&sortBy=&documentId=GALE%7CCX3441400088&windowstate=normal&activityType=&failOverType=&commentary="
+            +
+            "\nFuture of Interstellar Travel: http://www.space.com/13165-interstellar-travel-starship-destinations.html"
+        );
 }
