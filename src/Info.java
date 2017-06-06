7512f440be92abcf111e7a251c96a2fe6e813001
Will Anderson <wllmscttandrsn@gmail.com>
2017-06-06 08:22:59 -0400

    cleaned info up

--- src/Info.java
+++ src/Info.java
@@ -2,28 +2,15 @@ import javax.swing.*;
 
 public class Info
 {
-    private String query, info;
-    private static ImagePanel path = null;
+    private String info;
 
-    public Info(String query, String info){
+    public Info(String info){
         this.query = query;
         this.info = info;
 
-    }
-
-    public Info(String query, String info, ImagePanel path){
-        this.query = query;
-        this.info = info;
-        this.path = path;
-    }
-
-    public String getQuery(){
-        return query;
     }
 
     public String getInfo(){
         return info;
     }
-
-    public static ImagePanel getPath(){ return path; }
 }
