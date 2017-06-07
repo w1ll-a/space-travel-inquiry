481ff99700b049fe17cac99b73e5e7734b7e1def
Will Anderson <wllmscttandrsn@gmail.com>
2017-06-07 11:11:13 -0400

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -3,39 +3,44 @@ import javax.swing.*;
 public class Data {
     public static Info earlyTrav = new Info(
             "\tOn October 4, 1957, the Soviet satellite Sputnik I became the first artificial satellite in Earth’s atmosphere, followed by the US’s Explorer I in 1958. Explorer I had " +
-                    "an experiment from a physicist from the University of Iowa designed to scan for radiation zones in Earth’s atmosphere. Because of the success of the experiment, the US " +
-                    "Congress authorized the creation of the National Aeronautics and Space Agency (NASA) later that year. Early on in the Space Race during the Cold War, Russia was in the " +
-                    "lead in all fronts. The first man in space, Yuri A. Gagarin, was Russian. Aboard the Vostok 1, a modified SS-6 Sapwood ballistic missile, he reached a maximum altitude " +
-                    "of 203 miles above Earth and orbited Earth fully once in his 108-minute flight. The US’s first attempt at this was much less successful, with Alan Shepard Jr. reaching " +
-                    "a maximum altitude of only 116 miles and not completing a complete orbit around Earth in the space capsule Freedom 7. He received military flight pay of $14.38 for his " +
-                    "5,180 mph trip into space.");
+                "an experiment from a physicist from the University of Iowa designed to scan for radiation zones in Earth’s atmosphere. Because of the success of the experiment, the US " +
+                "Congress authorized the creation of the National Aeronautics and Space Agency (NASA) later that year. Early on in the Space Race during the Cold War, Russia was in the " +
+                "lead in all fronts. The first man in space, Yuri A. Gagarin, was Russian. Aboard the Vostok 1, a modified SS-6 Sapwood ballistic missile, he reached a maximum altitude " +
+                "of 203 miles above Earth and orbited Earth fully once in his 108-minute flight. The US’s first attempt at this was much less successful, with Alan Shepard Jr. reaching " +
+                "a maximum altitude of only 116 miles and not completing a complete orbit around Earth in the space capsule Freedom 7. He received military flight pay of $14.38 for his " +
+                "5,180 mph trip into space.");
 
     public static Info coldWar = new Info(
-            "\tThe Space Race, which was the race between the Soviet Union and the United States to the moon, was a major part of the Cold War that the United States ended up winning. " +
-                    "In the beginning, the Soviet Union was apparently ahead, with the launch of Sputnik I before the US’s Explorer I. However, after President John F. Kennedy’s famous speech " +
-                    "about space travel and its importance, space travel was prioritized much more and the US started gaining on the Soviet Union. The US’s Explorer I won over government " +
-                    "support, and later in 1958 the government authorized the creation of the National Aeronautics and Space Agency (NASA). The Space Race ultimately ended with the US moon " +
-                    "landing, where Neil Armstrong and Edwin “Buzz” Aldrin became the first men on the moon, and were returned to Earth safely as promised in Kennedy’s speech. However, what " +
-                    "the US put in time and money into getting to the moon and back, Soviet Union put into space stations. The Soviet government wanted to focus on space flight for spying " +
-                    "rather than manned space travel, so they tasked their scientists with designing a space station capable of housing multiple astronauts. The Soviet Union was also having "
-                    "political power struggles. This combined with the lack of government funding makes for an unsuccessful combination.");
+	    "\tThe Space Race, which was the race between the Soviet Union and the United States to the moon, was a major part of the Cold War that the United States ended up winning. " +
+                "In the beginning, the Soviet Union was apparently ahead, with the launch of Sputnik I before the US’s Explorer I. However, after President John F. Kennedy’s famous speech " +
+                "about space travel and its importance, space travel was prioritized much more and the US started gaining on the Soviet Union. The US’s Explorer I won over government " +
+                "support, and later in 1958 the government authorized the creation of the National Aeronautics and Space Agency (NASA). The Space Race ultimately ended with the US moon " +
+                "landing, where Neil Armstrong and Edwin “Buzz” Aldrin became the first men on the moon, and were returned to Earth safely as promised in Kennedy’s speech. However, what " +
+                "the US put in time and money into getting to the moon and back, Soviet Union put into space stations. The Soviet government wanted to focus on space flight for spying " +
+                "rather than manned space travel, so they tasked their scientists with designing a space station capable of housing multiple astronauts. The Soviet Union was also having "
+                "political power struggles. This combined with the lack of government funding makes for an unsuccessful combination.");
     
-	public static Info spaceShuttle = new Info(
-			"\tThe US wasn't done after the moon landing. The development of the space shuttle was a necessary undertaking, according to NASA. In a NASA document, three reasons for space shuttles " +
-					"were listed. Firstly, the space shuttle is a very cost-effective project, with its price low but its uses high. Secondly, a space shuttle would drastically lower costs to fly " +
-					"into space, because it would be reusable. It would be launched vertically from a launch pad exactly like a space ship, but instead of having to retrieve it from the ocean " +
-					"upon landing, it could glide down into the atmosphere and land on a runway like a normal plane. In fact, the only large difference between a space shuttle and a plane is " +
-					"that a plane uses a jet engine while a space shuttle would use rocket engines instead of jet engines. Thirdly, the space shuttle is needed to do useful things and makes " +
-					"sense in the long term. The space shuttle would not be a one-and-done deal, it would be reused for many years. Since it's so cheap, it opens up pathways that could not have " +
-					"been done using traditional space ships, and allows NASA to undertake more ambitious projects knowing that they can go to space and back for a miniscule cost, compared to the " +
-					"several space trips they'd have needed to have done with traditional space entry methods." +
-					"\n\tThe space shuttle program was launched officially in 1972, after President Richard Nixon announced that NASA planned to develop a \"multi-use spacecraft\" that would make " +
-					"space flight routine at a lower cost to the taxpayer."
-		);
+    public static Info spaceShuttle = new Info(
+            "\tThe US wasn't done after the moon landing. The development of the space shuttle was a necessary undertaking, according to NASA. In a NASA document, three reasons for space shuttles " +
+                "were listed. Firstly, the space shuttle is a very cost-effective project, with its price low but its uses high. Secondly, a space shuttle would drastically lower costs to fly " +
+                "into space, because it would be reusable. It would be launched vertically from a launch pad exactly like a space ship, but instead of having to retrieve it from the ocean " +
+                "upon landing, it could glide down into the atmosphere and land on a runway like a normal plane. In fact, the only large difference between a space shuttle and a plane is " +
+                "that a plane uses a jet engine while a space shuttle would use rocket engines instead of jet engines. Thirdly, the space shuttle is needed to do useful things and makes " +
+                "sense in the long term. The space shuttle would not be a one-and-done deal, it would be reused for many years. Since it's so cheap, it opens up pathways that could not have " +
+                "been done using traditional space ships, and allows NASA to undertake more ambitious projects knowing that they can go to space and back for a miniscule cost, compared to the " +
+                "several space trips they'd have needed to have done with traditional space entry methods." +
+                "\n\tThe space shuttle program was launched officially in 1972, after President Richard Nixon announced that NASA planned to develop a \"multi-use spacecraft\" that would make " +
+                "space flight routine at a lower cost to the taxpayer."
+            );
 	
     public static Info cassini = new Info(
-            "\t"
-        );
+            "\tThe Cassini program was a program designed to collect data about Saturn, more specifically its surface and atmosphere, as well as its moons. Cassini was the largest constructed " +
+                "interplanetary exploration device ever constructed by NASA. It launched October 15, 1997, and reached Saturn on June 30th, 2004. It remained in orbit of Saturn for another 10 " +
+                "years, collecting data and sending it back to Earth. On December 24, 2005, the Huygens probe was released and fell to Saturn's surface. The probe was designed to collect data " +
+                "about Saturn's surface and send it back to Earth, which it did. In 2017, just recently, the Grand Finale phase began, where Cassini plunged into Saturn's atmosphere, ending " +
+                "its 20 year journey." +
+                "\n\t"
+            );
     
     public static Info sources = new Info(
             "USH in context: http://ic.galegroup.com/ic/uhic/searchResults/actionWin?scanId=CSH&query=OQE+space+travel&prodId=UHIC&p=UHIC%3AWHIC&mode=view&catId=&u=colc95696&totalSearchResultCount=1931&limiter=&contentModules=&displayGroups=&display-query=OQE+space+travel&action=e&windowstate=normal&activityType=BasicSearch&resetBreadCrumb=true&failOverType=&commentary="
